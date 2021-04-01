2d014a2d170bfc5a9aea050454715dc2ccd60822
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2021-04-01 09:38:57 -0400

    Re-write without function calls is 30% faster!

--- 25.py
+++ 25.py
@@ -1,25 +1,14 @@
-# To continue, please consult the code grid in the manual.  Enter the code at row 2981, column 3075.
 Target = (2981, 3075)
-
-
-def next_code(code):
-    return (code * 252533) % 33554393
-
-
-def next_coord(c):
-    if c[0] == 1:
-        return (c[1] + 1, 1)
-    else:
-        return (c[0] - 1, c[1] + 1)
-
 
 code = 20151125
 coord = (1, 1)
 while True:
-    nextcoord = next_coord(coord)
-    nextcode = next_code(code)
     if coord == Target:
         print("Part 1: ", code)
         break
-    code = nextcode
-    coord = nextcoord
+
+    if coord[0] == 1:
+        coord = (coord[1] + 1, 1)
+    else:
+        coord = (coord[0] - 1, coord[1] + 1)
+    code = (code * 252533) % 33554393
